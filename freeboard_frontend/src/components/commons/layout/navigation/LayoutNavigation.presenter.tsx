89fa86cbfe6eda5a58546c605baec4050b04c4f9
woals7480 <94383932+woals7480@users.noreply.github.com>
2023-01-19 16:01:41 +0900

    firebase

--- freeboard_frontend/src/components/commons/layout/navigation/LayoutNavigation.presenter.tsx
+++ freeboard_frontend/src/components/commons/layout/navigation/LayoutNavigation.presenter.tsx
@@ -3,6 +3,7 @@ import * as S from "./LayoutNavigation.styles";
 import { ILayoutNavigationProps } from "./LayoutNavigation.type";
 
 const NAVIGATION_MENUS = [
+  { name: "나의파이어베이스", page: "/myfirebase" },
   { name: "라이브강아지", page: "/openapis" },
   { name: "라이브게시판", page: "/boards" },
   { name: "라이브상품", page: "/markets" },
