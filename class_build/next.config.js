bee44d4cc27512bbd7ab03924292effd4ed2f475
woals7480 <94383932+woals7480@users.noreply.github.com>
2023-03-29 22:43:17 +0900

    docker

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   generateBuildId: () => "codecamp_jjm_09",
+
+  // getServerSideProps 들어있는 페이지는 제외하고 빌드시켜줘!
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
